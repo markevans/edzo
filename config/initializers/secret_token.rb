ca9566cf67ac30d46cdd4b6b06eebc2cca2d727a
Mark Evans <mark@new-bamboo.co.uk>
2014-01-08 16:06:51 +0000

    first commit - empty rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edzo::Application.config.secret_key_base = '355ebd17a20598db9edac82c7ed8bed7ff4063ccc00f2fac2c2143587dda42e06b18d6ac67d2f5a9b7718ad8345f01a1d133584f53def3af50bb9f2e0fb7f388'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edzo::Application.config.session_store :cookie_store, key: '_edzo_session'
